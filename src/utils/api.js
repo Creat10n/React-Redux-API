f17a4933d3e614b97cce83b4278b47ae8dc75924
CREAT10N <nguyenluonghuy1996@gmail.com>
2018-10-03 19:36:35 +0700

    API call using axios

---
+++ src/utils/api.js
@@ -0,0 +1,43 @@
+import axios from "axios";
+
+const _baseURL = "http://api.openweathermap.org/data/2.5/";
+const _APIKEY = "49ddcce470c6a5f023b5c00012f07725";
+
+function prepRouteParams(queryStringData) {
+  return Object.keys(queryStringData)
+    .map(function(key) {
+      return key + "=" + encodeURIComponent(queryStringData[key]);
+    })
+    .join("&");
+}
+
+function prepUrl(type, queryStringData) {
+  return _baseURL + type + "?" + prepRouteParams(queryStringData);
+}
+
+function getQueryStringData(city) {
+  return {
+    q: city,
+    type: "accurate",
+    APPID: _APIKEY,
+    cnt: 5
+  };
+}
+
+export function getCurrentWeather(city) {
+  const queryStringData = getQueryStringData(city);
+  const url = prepUrl("weather", queryStringData);
+
+  return axios.get(url).then(function(currentWeatherData) {
+    return currentWeatherData.data;
+  });
+}
+
+export function getForcast(city) {
+  const queryStringData = getQueryStringData(city);
+  const url = prepUrl("forecast/daily", queryStringData);
+
+  return axios.get(url).then(function(forecastData) {
+    return forecastData.data;
+  });
+}
